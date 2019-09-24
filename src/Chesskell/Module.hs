c6a99ec16d703178f5031e3db30f3e29b2cd56b1
Maxim <m.a.tereshchuk@gmail.com>
2019-09-24 20:24:14 +0300

    Module error handler added

--- src/Chesskell/Module.hs
+++ src/Chesskell/Module.hs
@@ -3,6 +3,8 @@ module Chesskell.Module
   ) where
 
 import           System.Environment     (getArgs)
+import           Control.Exception      (ErrorCall, try, evaluate)
+import           Data.Vector            (Vector)
 import qualified Graphics.Gloss         as UI
 import           Chesskell.Chess        (Piece)
 import           Chesskell.CoreCommons  (Game, AppState (..))
@@ -11,14 +13,45 @@ import           Chesskell.Preprocessor (preprocessRawGames)
 import           Chesskell.View         (chesskellDisplay, backgroundColor, getStaticPic, getPieceToPicMap, appStateToPic)
 import           Chesskell.Controller   (updateAppState)
 
+process :: IO (Either String AppState)
+process = do
+  args <- getArgs
+  if null args
+  then return $ Left missingFilePath
+  else do
+    let filePath = head args
+    parseRes     <- parsePGNFile filePath
+    case parseRes of
+      (Left parseErrorReason) -> return $ Left $ parseError ++ show parseErrorReason
+      (Right rawGames)  -> do
+        preprocessRes <- try $ evaluate $ preprocessRawGames rawGames :: IO (Either ErrorCall (Vector Game))
+        case preprocessRes of
+          (Left _)      -> return $ Left invalidMove  
+          (Right games) -> do
+            pieceToPicMap <- getPieceToPicMap
+            return $ Right AppState
+              { _staticPic = getStaticPic
+              , _pieceToPicMap = pieceToPicMap
+              , _games = games
+              , _gameNum = 0
+              , _moveNum = 0
+              }
+  where
+    missingFilePath = "Missing file path"
+    parseError      = "Error during parsing file: "
+    invalidMove     = "Invalid move detected"
+
 run :: IO ()
 run = do
-  args <- getArgs
-  let filePath = head args
-  parseRes <- parsePGNFile filePath
-  let (Right rawGames) = parseRes
-  let games = preprocessRawGames rawGames
-  pieceToPicMap <- getPieceToPicMap
-  let initialAppState =
-        AppState {_staticPic = getStaticPic, _pieceToPicMap = pieceToPicMap, _games = games, _gameNum = 0, _moveNum = 0}
-  UI.play chesskellDisplay backgroundColor 10 initialAppState appStateToPic updateAppState (const id) 
+  processRes <- process
+  case processRes of
+    (Left errorMessage)     -> putStrLn errorMessage 
+    (Right initialAppState) -> 
+      UI.play
+        chesskellDisplay
+        backgroundColor
+        0
+        initialAppState
+        appStateToPic
+        updateAppState
+        (const id)
